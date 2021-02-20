55aa139a9ea6b467d4dc1fcf7e82c14e77292fd2
tonyw0527 <tonyw0527@gmail.com>
2021-02-21 01:00:09 +0900

    Update app.ts & README.md

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 This is a easy crawling example with Cheerio, Axios and Typescript.
 
-# How to use
+## Functions
+
+- delay(ms: number)
+  Set delay to the http request
+
+- getHTML(url: string)
+  Get html data from the URL
+
+- getSomeThing(..args: string)
+  Custom function for handling queries or parameters.
+
+- saveInJSON(fileName: string, data: Array<object>)
+  Save the data into JSON file.
+
+## How to use
 
 ```sh
 git clone
--- src/app.ts
+++ src/app.ts
@@ -23,17 +23,38 @@ function getHTML(url: string) {
   })
 }
 
-// custom function
-async function getSomeThing(name: string) {
+// custom function for handling queries or parameters.
+async function getSomeThing(name: string, city: string) {
 
-  const baseURL = `https://people.com/list?name=${name}`;
+  const baseURL = `https://people.com/list?name=${name}&city=${city}`;
 
   return await getHTML(baseURL).then((html: any) => {
     
     // whole data
     const $ = cheerio.load(html.data);
   
-    // pick a data you need in jquery style
+    // pick a data you need by jquery grammar
     const userName = $(`#${name}`, 'body').find('.name-span').text();
+    console.log('crawled user name - ', userName);
   })
+}
+
+// If you want to save the data into JSON file.
+async function saveInJSON(fileName: string, data: Array<object>) {
+  
+  const json = JSON.stringify(data);
+
+  fs.writeFile(`${fileName}.json`, '', 'utf8', (error: any) => {
+    if (error) {
+      console.log('error', error);
+    }
+    console.log(`Successfully created ${fileName}.json`);
+  });
+
+  try {
+    fs.appendFileSync(`${fileName}.json`, json, 'utf8');
+    console.log('Successfully saved in json');
+  } catch (error: any) {
+    console.log('error', error);
+  }
 }
